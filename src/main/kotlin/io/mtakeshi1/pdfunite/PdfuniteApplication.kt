5c2b0f9c026ac6b9fea8b36ea52192c7bf8a2919
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2024-05-30 13:16:30 -0300

    initial commit

---
+++ src/main/kotlin/io/mtakeshi1/pdfunite/PdfuniteApplication.kt
@@ -0,0 +1,11 @@
+package io.mtakeshi1.pdfunite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PdfuniteApplication
+
+fun main(args: Array<String>) {
+	runApplication<PdfuniteApplication>(*args)
+}
